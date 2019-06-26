026bdf5142d2ceca6d5bce833d999e756ae52550
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-06-26 09:53:47 +0200

    clean

--- Sprints/Sprint1temp/it.unibo.sprint1/sprint1.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% sprint1 description   
-%====================================================================================
-mqttBroker("localhost", "1883").
-context(ctxmindsprint1, "localhost",  "MQTT", "0" ).
- qactor( resourcemodel, ctxmindsprint1, "external").
-  qactor( butler, ctxmindsprint1, "it.unibo.butler.Butler").
-  qactor( butlerstep, ctxmindsprint1, "it.unibo.butlerstep.Butlerstep").
-  qactor( planner, ctxmindsprint1, "it.unibo.planner.Planner").
