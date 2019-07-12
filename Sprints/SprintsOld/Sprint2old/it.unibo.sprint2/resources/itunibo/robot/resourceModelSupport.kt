27be39deecb68b7aeabe3e4252fb38798f83284e
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-07-12 18:29:39 +0200

    sprint2 test work (need do the test behaviour)

---
+++ Sprints/SprintsOld/Sprint2old/it.unibo.sprint2/resources/itunibo/robot/resourceModelSupport.kt
@@ -0,0 +1,37 @@
+package itunibo.robot
+
+import it.unibo.kactor.ActorBasic
+import kotlinx.coroutines.launch
+import itunibo.coap.modelResourceCoap
+
+object resourceModelSupport{
+lateinit var resourcecoap : modelResourceCoap
+	
+	fun setCoapResource( rescoap : modelResourceCoap ){
+		resourcecoap = rescoap
+	}
+	
+	fun updateRobotModel( actor: ActorBasic, content: String ){
+ 			actor.solve(  "action(robot, move($content) )" ) //change the robot state model
+			actor.solve(  "model( A, robot, STATE )" )
+			val RobotState = actor.getCurSol("STATE")
+			//println("			resourceModelSupport updateModel RobotState=$RobotState")
+			actor.scope.launch{
+ 				actor.emit( "modelChanged" , "modelChanged(  robot,  $content)" )  //for the robotmind
+				actor.emit( "modelContent" , "content( robot( $RobotState ) )" )
+				resourcecoap.updateState( "robot( $RobotState )" )
+  			}	
+	}	
+	fun updateSonarRobotModel( actor: ActorBasic, content: String ){
+ 			actor.solve( "action( sonarRobot,  $content )" ) //change the robot state model
+			actor.solve( "model( A, sonarRobot, STATE )" )
+			val SonarState = actor.getCurSol("STATE")
+			//println("			resourceModelSupport updateSonarRobotModel SonarState=$SonarState")
+			actor.scope.launch{
+ 				actor.emit( "modelContent" , "content( sonarRobot( $SonarState ) )" )
+				resourcecoap.updateState( "sonarRobot( $SonarState )" )
+ 			}	
+	}	
+ 	
+}
+
