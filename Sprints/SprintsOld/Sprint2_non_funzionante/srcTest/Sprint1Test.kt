b90636a0f59db1c5f60019f8fbda465d984be425
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-07-13 17:37:04 +0200

    recreate sprint2

---
+++ Sprints/SprintsOld/Sprint2_non_funzionante/srcTest/Sprint1Test.kt
@@ -0,0 +1,50 @@
+package it.unibo.sprint1a
+
+import org.junit.Before
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.junit.Test
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.sysUtil
+import org.junit.runner.RunWith
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+
+
+class Sprint1Test {
+ 	var resource : ActorBasic? = null
+	
+
+	@Before
+	fun systemSetUp() {
+  	 		GlobalScope.launch{ //activate an observer ...
+ 				itunibo.coap.observer.main()	//blocking
+ 			}	
+  	 		GlobalScope.launch{
+				println(" %%%%%%% TestMockup starts resource model ")
+				it.unibo.ctxResourceModel.main()
+ 			}
+  	 		GlobalScope.launch{
+				println(" %%%%%%% TestMockup starts robot mind ")
+				it.unibo.ctxRobot.main()
+ 			}
+		
+			delay(5000)		//give the time to start
+			resource = sysUtil.getActor("resourcemodel")	
+		    println(" %%%%%%% TestMockup getActors resource=${resource}")
+ 	}
+ 
+	@After
+	fun terminate() {
+		println(" %%%%%%% TestMockup terminate ")
+	}
+ 
+	@Test
+	fun moveTest() {
+		println(" %%%%%%% TestMockup  moveTest ")
+ 	}
+
+	fun delay( time : Long ){
+		Thread.sleep( time )
+	}
+}
