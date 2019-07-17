a59ba08a75e4fa930e2782294cb4d16a0ca9cb34
FerrariAndrea <ferrari.andrea.95.b10@gmail.com>
2019-07-17 15:21:37 +0200

    toolForReal

---
+++ Altro/RobotPinger/RobotPinger/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobotPinger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += closing;
+            trackBar1.Value = Properties.Settings.Default.interval;
+            textBox1.Text=Properties.Settings.Default.mac_pc;
+            textBox2.Text=Properties.Settings.Default.mac_robot ;
+            timer1.Interval = trackBar1.Value;
+            timer1.Start();
+        }
+        private void closing(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            Properties.Settings.Default.mac_pc = textBox1.Text;
+                     
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.mac_robot = textBox2.Text;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Check(textBox1.Text.ToLower().Replace(':', '-'), ref _labelRobot);
+            Check(textBox2.Text.ToLower().Replace(':', '-'), ref _labelPC);
+        }
+   
+        public void Check(string Name,ref Label l)
+        {
+      
+            try
+            {
+
+                string ris = ArpHelper.FindIpAddressByMacAddress(Name);
+                if (ris != null)
+                {
+                    l.Text = "On--> " + ris;
+                    l.ForeColor = Color.DarkGreen;
+                }
+                else
+                {
+                    l.Text = "Offline";
+                    l.ForeColor = Color.Orange;
+                }
+
+               
+            }
+            catch (Exception e)
+            {
+                l.Text = "Errore: " + e.Message;
+                l.ForeColor = Color.DarkRed;
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            _labelInterval.Text = trackBar1.Value + "ms";
+            timer1.Interval = trackBar1.Value;
+            Properties.Settings.Default.interval= trackBar1.Value;
+        }
+    }
+}
