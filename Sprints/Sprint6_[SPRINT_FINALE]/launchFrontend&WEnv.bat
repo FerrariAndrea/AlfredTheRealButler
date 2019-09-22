2ed82991eced9bc3c01360822dc762e716b34460
Cesarsk <luca.cesarano1@gmail.com>
2019-09-22 18:13:45 +0200

    Repo Organized

--- Sprints/Sprint6_[SPRINT_FINALE]/launchFrontend&WEnv.bat
+++ Sprints/Sprint6_[SPRINT_FINALE]/launchFrontend&WEnv.bat
@@ -1,6 +1,7 @@
 cd ..
-cd ..
+cd "it.unibo.robots19_[VIRTUAL_ENVIRONMENT]"
 start cmd /k startVirtualEnvironment.bat
+cd ..
 cd Sprints
-cd Sprint6
+cd "Sprint6_[SPRINT_FINALE]"
 start cmd /k launchFrontend.bat
--- Sprints/it.unibo.robots19_[VIRTUAL_ENVIRONMENT]/startVirtualEnvironment.bat
+++ Sprints/it.unibo.robots19_[VIRTUAL_ENVIRONMENT]/startVirtualEnvironment.bat
@@ -1,5 +1,3 @@
-cd Sprints
-cd it.unibo.robots19
 cd node
 cd WEnv
 cd server
