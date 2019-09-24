1e25e1929e1a49b2da6366f06bea312c924bea57
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-09-24 13:06:52 +0200

    up

--- Sprints/Sprint6/it.unibo.sprint6/system1.pl
+++ Sprints/Sprint6/it.unibo.sprint6/system1.pl
@@ -2,12 +2,6 @@
 % system1 description   
 %====================================================================================
 mqttBroker("localhost", "1883").
-context(ctxrobot, "localhost",  "MQTT", "0" ).
-context(ctxdummy, "dummyhost",  "MQTT", "0" ).
- qactor( kb, ctxdummy, "external").
-  qactor( resourcemodel, ctxdummy, "external").
-  qactor( mindrobot, ctxrobot, "it.unibo.mindrobot.Mindrobot").
-  qactor( basicrobot, ctxrobot, "it.unibo.basicrobot.Basicrobot").
-  qactor( sonarhandler, ctxrobot, "it.unibo.sonarhandler.Sonarhandler").
-  qactor( onerotateforward, ctxrobot, "it.unibo.onerotateforward.Onerotateforward").
-  qactor( onecellforward, ctxrobot, "it.unibo.onecellforward.Onecellforward").
+context(ctx, "localhost",  "MQTT", "0" ).
+ qactor( cliente, ctx, "it.unibo.cliente.Cliente").
+  qactor( server, ctx, "it.unibo.server.Server").
