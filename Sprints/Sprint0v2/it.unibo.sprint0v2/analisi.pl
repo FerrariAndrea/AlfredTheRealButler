453db3f943b09bd8d99eaab0a09c5a8f66604bd1
AndreaFerrari <andrea.ferrari24@studio.unibo.it>
2019-09-24 16:06:47 +0200

    fix sprint0

---
+++ Sprints/Sprint0v2/it.unibo.sprint0v2/analisi.pl
@@ -0,0 +1,13 @@
+%====================================================================================
+% analisi description   
+%====================================================================================
+mqttBroker("localhost", "1883").
+context(ctxrobot, "localhost",  "MQTT", "0" ).
+context(ctxmaitre, "localhosta",  "MQTT", "0" ).
+context(ctxresourcemodel, "localhostb",  "MQTT", "0" ).
+ qactor( maitre, ctxmaitre, "it.unibo.maitre.Maitre").
+  qactor( basicrobot, ctxrobot, "it.unibo.basicrobot.Basicrobot").
+  qactor( mindrobot, ctxrobot, "it.unibo.mindrobot.Mindrobot").
+  qactor( fridge, ctxrobot, "it.unibo.fridge.Fridge").
+  qactor( resourcemodel, ctxresourcemodel, "it.unibo.resourcemodel.Resourcemodel").
+  qactor( kb, ctxresourcemodel, "it.unibo.kb.Kb").
---
+++ Sprints/Sprint0v2/it.unibo.sprint0v2/srcTest/Sprint0Test.kt
@@ -0,0 +1,81 @@
+package it.unibo.sprint0v2
+
+import org.junit.Before
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.junit.Test
+import it.unibo.kactor.ActorBasic
+import it.unibo.kactor.sysUtil
+import org.junit.runner.RunWith
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import it.unibo.kactor.MsgUtil
+import it.unibo.blsFramework.kotlin.fsm.forward
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.junit.BeforeClass
+
+
+class Sprint0Test {
+
+
+	companion object {
+		//----------------------var
+		var testName = "SprintTest"
+		var actor : ActorBasic?=null
+		init {}
+
+//-------------------------------------------------------UTILS for test
+	fun delay( time : Long ){
+		Thread.sleep( time )
+	}
+
+	
+
+	//-----------------------------------------------------END utils for test
+		
+		//start all
+		@BeforeClass @JvmStatic
+		fun systemSetUp() {
+			println("Start test as Maitre")
+			delay(250)
+			GlobalScope.launch{
+				it.unibo.ctxMaitre.main()
+				
+			}
+			delay(500)
+			GlobalScope.launch{
+				actor=sysUtil.getActor("maitre")
+				println(actor!!.name)
+				
+			}
+		
+			
+		}
+
+	}
+
+
+
+	
+	@Test
+	fun fridgeTest(){
+		println("-----")
+		
+		println("-----")
+		GlobalScope.launch{
+			println(actor!!.name)
+			var message: MqttMessage=MqttMessage()
+			actor!!.messageArrived("content",message)
+			println(message.toString())
+		}
+		 
+		
+	}
+
+	
+
+
+
+}
